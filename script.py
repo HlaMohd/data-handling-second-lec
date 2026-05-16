ce0122d8ba85ad902b9d0662a5c2d526e8ddcc05
unknown <hallamhdafana2002@gmail.com>
2026-05-16 13:29:28 +0300

    dawnloaded the parquet library used it in the program, readed the file, print sample data from the file

--- script.py
+++ script.py
@@ -2,9 +2,12 @@ import pandas as pd
 df_CSV = pd.read_csv('session2_data/student_coffee_crisis.csv')
 df_XLSX = pd.read_excel('session2_data/student_coffee_crisis.xlsx')
 df_JSON = pd.read_json('session2_data/student_coffee_crisis.json')
+df_parquet = pd.read_parquet('session2_data/student_coffee_crisis.parquet')
 print(df_CSV.sample(10))
 print(df_XLSX.sample(10))
 print(df_JSON.sample(10))
+print("parquet data",df_parquet.sample(10))
+
 #import os
 #print(os.listdir())
 #print(os.listdir())
